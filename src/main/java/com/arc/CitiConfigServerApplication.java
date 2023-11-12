60d18a1d397273157ad52ee99769fb41a84fd0be
Adi Chirde <adichirde@Adis-MacBook-Air.local>
2023-11-12 10:31:47 +0530

    first commit

---
+++ src/main/java/com/arc/CitiConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.arc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiConfigServerApplication.class, args);
+	}
+
+}
